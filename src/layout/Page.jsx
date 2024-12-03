df9b9e004e2a413a956ed7ed2784da07ed5592bb
nicola.manfredi.210796 <nicola.manfredi.210796@gmail.com>
2024-12-03 09:56:15 +0000

    set up layout

---
+++ src/layout/Page.jsx
@@ -0,0 +1,13 @@
+
+function Page() {
+    const omelette = 'src/assets/image-omelette.jpeg';
+
+    return (
+        <main>
+            <img src={omelette} alt="image-omelette.jpeg" />
+            
+        </main>
+    );
+}
+
+export default Page;
